bc5bec1da161a039ad0b0bac2e7c3f502a34cef9
Sebastian Montero <smontero@mightywizards.com>
2019-05-03 11:27:43 -0500

    Exposed ExchangeOrderInterpreter.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const GyftieListener = require('./src/GyftieListener');
 const { OrderTypes } = require('./src/const');
+const { ExchangeOrderInterpreter } = require('./src/interpreters');
 
 module.exports = {
     GyftieListener,
-    OrderTypes
+    OrderTypes,
+    ExchangeOrderInterpreter,
 };
