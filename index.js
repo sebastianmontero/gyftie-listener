35148caaa693209653be2e8b769b8eb25a8db422
Sebastian Montero <smontero@mightywizards.com>
2019-05-06 10:57:38 -0500

    Exposed ExchangeTradeInterpreter

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const GyftieListener = require('./src/GyftieListener');
 const { OrderTypes, ActionTraceKeys } = require('./src/const');
-const { ExchangeOrderInterpreter } = require('./src/interpreters');
+const { ExchangeOrderInterpreter, ExchangeTradeInterpreter } = require('./src/interpreters');
 const ActionTraceFactory = require('./src/ActionTraceFactory');
 module.exports = {
     GyftieListener,
     OrderTypes,
     ExchangeOrderInterpreter,
+    ExchangeTradeInterpreter,
     ActionTraceFactory,
     ActionTraceKeys
 };
